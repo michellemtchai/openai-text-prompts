bf5f40560ab62bb4ff270021e9663f42ba9211c1
michellemtchai <michellemtchai@gmail.com>
2024-08-25 10:11:17 -0400

    catch error in response

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ const App = () => {
       const data = await res.json();
       const { msg } = data;
       setLoading(false);
-      if (res.status === 200) {
+      if (res.status === 200 && !msg.error) {
         addToLog(prompt, msg);
         setPrompt('');
       } else {
